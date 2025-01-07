a789797e171d80f0f9b292992108d9f6d6af0dc0
Aleksey Yurasov <alex286ur@rambler.ru>
2025-01-08 01:20:11 +0300

    add game brain-progression

---
+++ src/games/brain-progression.js
@@ -0,0 +1,33 @@
+import mainEngine from '../index.js';
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const description = 'What number is missing in the progression?';
+
+const getProgressionArray = (firstItem, step, length) => {
+    const progressionArray = [];
+
+    for (let i = 0; i < length; i += 1) {
+        progressionArray.push(firstItem + step * i);
+    }
+    return progressionArray;
+};
+
+const getData = () => {
+    const firstItem = getRandomNumber(1, 10);
+    const step = getRandomNumber(1, 10);
+    const length = getRandomNumber(5, 10);
+    const progression = getProgressionArray(firstItem, step, length);
+    const indexOfInvisNum = getRandomNumber(0, length - 1);
+
+    const rightAnswer = `${progression[indexOfInvisNum]}`;
+    progression[indexOfInvisNum] = '..';
+    const question = progression.join(' ');
+    return [question, rightAnswer];
+}
+
+const startGameProgression = () => mainEngine(getData, description);
+
+export default startGameProgression;
